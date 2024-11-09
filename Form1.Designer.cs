786b972e7ece3b81798c9801957431b31d5ca1b3
sally4952 <maxim4953@outlook.com>
2024-11-09 17:56:32 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace 自动答案生成器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(23, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "自动答案生成器";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "题数";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(177, 21);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "选项数";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(59, 69);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(177, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(171, 209);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(65, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "退出";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(224, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "生成答案";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 125);
+            this.textBox3.MaxLength = 100000;
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(224, 47);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 209);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "复制到剪切板";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(105, 213);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(60, 16);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "带空格";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(15, 183);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(84, 16);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "多线程执行";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(248, 244);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "自动答案生成器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 自动答案生成器
+{
+    public partial class Form1 : Form
+    {
+        public static uint 题数;
+        public static uint 选项数;
+        public static string 答案;
+        public static Random randow = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        [STAThread]
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                题数 = uint.Parse(textBox1.Text);
+                if (题数 == 0) throw new Exception("题数不能等于0");
+                if (题数 * 3 > 100000) throw new Exception("题数不能大于100000");
+            }
+            catch
+            {
+                MessageBox.Show("题目数量无效，请重新输入！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Focus();
+                return;
+            }
+            try
+            {
+                选项数 = uint.Parse(textBox2.Text);
+                if (选项数 == 0) throw new Exception("选项数不能等于0");
+                if (选项数 > 26) throw new Exception("选项数不能大于26");
+            }
+            catch
+            {
+                MessageBox.Show("选项数量无效，请重新输入！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox2.Focus();
+                return;
+            }
+            if (!checkBox2.Checked)
+            {
+                for (int i = 0; i < 题数; i++)
+                {
+                    int temp = randow.Next(1, (int)(选项数 + 1));
+                    if (temp == 1) 答案 += "A";
+                    else if (temp == 2) 答案 += "B";
+                    else if (temp == 3) 答案 += "C";
+                    else if (temp == 4) 答案 += "D";
+                    else if (temp == 5) 答案 += "E";
+                    else if (temp == 6) 答案 += "F";
+                    else if (temp == 7) 答案 += "G";
+                    else if (temp == 8) 答案 += "H";
+                    else if (temp == 9) 答案 += "I";
+                    else if (temp == 10) 答案 += "J";
+                    else if (temp == 11) 答案 += "K";
+                    else if (temp == 12) 答案 += "L";
+                    else if (temp == 13) 答案 += "M";
+                    else if (temp == 14) 答案 += "N";
+                    else if (temp == 15) 答案 += "O";
+                    else if (temp == 16) 答案 += "P";
+                    else if (temp == 17) 答案 += "Q";
+                    else if (temp == 18) 答案 += "R";
+                    else if (temp == 19) 答案 += "S";
+                    else if (temp == 20) 答案 += "T";
+                    else if (temp == 21) 答案 += "U";
+                    else if (temp == 22) 答案 += "V";
+                    else if (temp == 23) 答案 += "W";
+                    else if (temp == 24) 答案 += "X";
+                    else if (temp == 25) 答案 += "Y";
+                    else if (temp == 26) 答案 += "Z";
+                    else throw new Exception("我代码是不是被改了？！");
+
+                    答案 += "  ";
+                }
+                textBox3.Text = 答案;
+                答案 = "";
+            }
+            else
+            {
+                Task[] tasks = new Task[10];
+                for (int i = 0;i < tasks.Length;i++)
+                {
+                    tasks[i] = Task.Run(() =>
+                    {
+                        for (int j = 0; j < (题数 / 10); j ++)
+                        {
+                            int temp = randow.Next(1, (int)(选项数 + 1));
+                            if (temp == 1) 答案 += "A  ";
+                            else if (temp == 2) 答案 += "B  ";
+                            else if (temp == 3) 答案 += "C  ";
+                            else if (temp == 4) 答案 += "D  ";
+                            else if (temp == 5) 答案 += "E  ";
+                            else if (temp == 6) 答案 += "F  ";
+                            else if (temp == 7) 答案 += "G  ";
+                            else if (temp == 8) 答案 += "H  ";
+                            else if (temp == 9) 答案 += "I  ";
+                            else if (temp == 10) 答案 += "J  ";
+                            else if (temp == 11) 答案 += "K  ";
+                            else if (temp == 12) 答案 += "L  ";
+                            else if (temp == 13) 答案 += "M  ";
+                            else if (temp == 14) 答案 += "N  ";
+                            else if (temp == 15) 答案 += "O  ";
+                            else if (temp == 16) 答案 += "P  ";
+                            else if (temp == 17) 答案 += "Q  ";
+                            else if (temp == 18) 答案 += "R  ";
+                            else if (temp == 19) 答案 += "S  ";
+                            else if (temp == 20) 答案 += "T  ";
+                            else if (temp == 21) 答案 += "U  ";
+                            else if (temp == 22) 答案 += "V  ";
+                            else if (temp == 23) 答案 += "W  ";
+                            else if (temp == 24) 答案 += "X  ";
+                            else if (temp == 25) 答案 += "Y  ";
+                            else if (temp == 26) 答案 += "Z  ";
+                            else throw new Exception("我代码是不是被改了？！");
+                        }
+                    });
+                }
+                await Task.WhenAll(tasks);
+                textBox3.Text = 答案;
+                答案 = "";
+            }
+        }
+
+        [STAThread]
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) Clipboard.SetDataObject(textBox3.Text);
+            else Clipboard.SetDataObject(textBox3.Text.Replace(" ", ""));
+            MessageBox.Show("复制成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked && (MessageBox.Show("此功能暂时不稳定，确定开启吗？", "提示", MessageBoxButtons.OKCancel, 
+                MessageBoxIcon.Question) == DialogResult.OK)) checkBox2.Checked = true;
+            else checkBox2.Checked = false;
+        }
+    }
+}
